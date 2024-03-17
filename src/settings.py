872a0306fbb02e5d9f0565293b04b0a871416fe9
Ross Tsenov <ross.tsenov@gmail.com>
2024-03-17 02:03:09 +0000

    Update settings

--- src/settings.py
+++ src/settings.py
@@ -1,19 +1,38 @@
-from enum import StrEnum
+from enum import StrEnum, auto
+from typing import Literal
 
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
+__all__ = [
+    "Environment",
+    "settings",
+]
+
 
 class Environment(StrEnum):
-    DEV = "dev"
-    PROD = "prod"
+    DEV = auto()
+    PROD = auto()
+
+
+LoggingLevel = Literal[
+    "CRITICAL",
+    "FATAL",
+    "ERROR",
+    "WARNING",
+    "WARN",
+    "INFO",
+    "DEBUG",
+    "NOTSET",
+]
 
 
 class Settings(BaseSettings):
     """Global application settings loaded from the .env file."""
 
     env: Environment = Environment.DEV
-    other_var: str = "other"
+    logger_name: str = "project_logger"
+    logging_level: LoggingLevel = "INFO"
 
 
 load_dotenv(override=True)
